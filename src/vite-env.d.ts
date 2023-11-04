fe9812b0e6e50e0af625dadb5dd604d8131f70c8
Alvagit25 <alvarodesegovia@gmail.com>
2023-11-04 12:29:45 +0100

    añadir .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_APIKEY: string
+    // more env variables...
+  }
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,10 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit:{
+    env:{
+      dir:'./',
+      publicPrefix: ''
+    }
+  }
 }
